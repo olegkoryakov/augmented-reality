a082ee215de7196d1fa40fd6498ded671869ce9a
oleg koryakov <outflowpwnz@gmail.com>
2021-08-29 17:28:12 +0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="aframe-wrapper"
+  >
+    <a-scene embedded arjs>
+      <!-- <a-sphere
+        position="0 1.25 -5"
+        radius="1.24"
+        color="red"
+      /> -->
+      <a-marker
+        preset="hiro"
+      >
+        <a-box color="#ff0" scale="0.5 0.5 0.5" material="opacity: 0.5"></a-box>
+      </a-marker>
+      <a-entity camera>
+        <a-cursor></a-cursor>
+      </a-entity>
+    </a-scene>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+require('./utils/aframe/aframe.min.js')
+
+createApp(App).mount('#app')
